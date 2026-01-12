6a8e9789a33bf5741e5d86b65a57ec7fe11cdafa
Lucas Vieira <lucasvieira.dev2020@gmail.com>
2026-01-12 19:41:31 -0300

    Change environ access to use get method

--- src/app/config.py
+++ src/app/config.py
@@ -7,26 +7,26 @@ from os import path, environ
 
 class Config:
     PYTHONPATH = "src"
-    MONGO_URI = environ["MONGO_URI"]
-    SECRET_KEY = environ["SECRET_KEY"]
-    STRIPE_SECRET_KEY = environ["STRIPE_SECRET_KEY"]
-    PRICE_ID = environ['PRICE_ID']
-    STRIPE_WHSEC = environ["STRIPE_WHSEC"]
-    GENAI_API_KEY = environ["GENAI_API_KEY"]
-    GENAI_MODEL = environ["GENAI_MODEL"]
+    MONGO_URI = environ.get("MONGO_URI")
+    SECRET_KEY = environ.get("SECRET_KEY")
+    STRIPE_SECRET_KEY = environ.get("STRIPE_SECRET_KEY")
+    PRICE_ID = environ.get('PRICE_ID')
+    STRIPE_WHSEC = environ.get("STRIPE_WHSEC")
+    GENAI_API_KEY = environ.get("GENAI_API_KEY")
+    GENAI_MODEL = environ.get("GENAI_MODEL")
 
-    MAIL_SERVER = environ["MAIL_SERVER"]
-    MAIL_PORT = environ["MAIL_PORT"]
+    MAIL_SERVER = environ.get("MAIL_SERVER")
+    MAIL_PORT = environ.get("MAIL_PORT")
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = environ["MAIL_USERNAME"]
-    MAIL_PASSWORD = environ["MAIL_PASSWORD"]
-    MAIL_DEFAULT_SENDER = environ["MAIL_DEFAULT_SENDER"]
+    MAIL_USERNAME = environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = environ.get("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = environ.get("MAIL_DEFAULT_SENDER")
     
-    FRONT_BASE_URL = environ["FRONT_BASE_URL"]
+    FRONT_BASE_URL = environ.get("FRONT_BASE_URL")
 
     FLASK_ENV = "development"
-    PORT = int(environ["PORT"])
+    PORT = int(environ.get("PORT"))
 
 
 
